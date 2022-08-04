e9102c43ac5b512f840edf00b0dc2495a6cb469a
ThejanThrimanna <thejan.dthrimanna@gmail.com>
2022-08-05 00:47:12 +0530

    API params added as the Constants in helper

--- app/src/main/java/com/thejan/assessment/digikraft_assignment/helper/Constants.kt
+++ app/src/main/java/com/thejan/assessment/digikraft_assignment/helper/Constants.kt
@@ -1,3 +1,7 @@
 package com.thejan.assessment.digikraft_assignment.helper
 
 const val BASE_URL = "http://www.poznan.pl/mim/plan/"
+
+//API Params
+const val TYPE = "pub_transport"
+const val CO = "stacje_rowerowe"
